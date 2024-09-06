8e308ceb7b96ccfd556ffd4fcb924cbde68947d9
kassem00 <kamsdonga@gmail.com>
2024-09-06 23:03:22 +0000

    au

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,16 +37,6 @@ class BasicAuth(Auth):
 
         return (credentials[0], credentials[1])
 
-    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
-        """
-        Returns object credentials
-        """
-        if user_email is None or\
-           not isinstance(user_email, str):
-            return None
-        if user_pwd is None or\
-           not isinstance(user_pwd, str):
-            return None
 
     def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """

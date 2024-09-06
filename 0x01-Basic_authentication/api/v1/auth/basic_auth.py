5fe8652a01fd35b18a5ee97275eba518952dceb4
kassem00 <kamsdonga@gmail.com>
2024-09-06 20:54:10 +0000

    au

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,7 @@ BasicAuth class
 """
 
 from api.v1.auth.auth import Auth
-
+import base64
 
 class BasicAuth(Auth):
     """ BasicAuth class """
@@ -19,3 +19,20 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(" ")[1]
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Returns the user email and password from the Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None or\
+           not isinstance(decoded_base64_authorization_header, str) or\
+           ":" not in decoded_base64_authorization_header:
+            return (None, None)
+
+        credentials = decoded_base64_authorization_header.split(":")
+
+
+        if len(credentials) != 2:
+            return (None, None)
+
+        return (credentials[0], credentials[1])

ee265ce37fcea6adb50cdafb9f58e1e9d8a9a8a2
kassem00 <kamsdonga@gmail.com>
2024-09-14 09:58:57 +0000

    au

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -17,10 +17,10 @@ class User(Base):
     reset_token, a nullable string
     """
     id = Column(Integer, primary_key=True)
-    email = Column(String(255), nullable=False)
-    hashed_password = Column(String(255), nullable=False)
-    session_id = Column(String(255), nullable=True)
-    reset_token = Column(String(255), nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
 
     def __repr__(self):
         return "<User(name='%s', fullname='%s', nickname='%s')>" % (
